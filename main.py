6b758aa85390588065657c3d59fdfeb57cd772d2
SimonBruun <bsb.bruun@hotmail.com>
2019-08-19 10:56:18 +0200

    Digt

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+print("""Der findes på adskillige kvinder
+flanken lukker åbner kommer
+husmurene erindring findes videre
+et drømmer sneens morgen
+til endnu deles munden
+øjnene efterligner møde og støv
+alt let forsvar drejer ved hundene
+men dagning og kærlighed eller kravlen og kravlen
+er stærkere uden det et-sagte land
+med organerne endnu ligger organer
+personer personer hvad enten
+de leveres og ikke indføres simpelthen
+sproget er ikke lidt når
+fraværet da er ophidselsens svar.""")
