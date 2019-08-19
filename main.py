75836bf94b521c07b879a52b49e3a6caf9196cdc
Razeonic <Razeonic@gmail.com>
2019-08-19 11:03:23 +0200

    yeet

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-print("""Der findes på adskillige kvinder
+print("""Der findes på Mange kvinder
 flanken lukker åbner kommer
 husmurene erindring findes videre
-et drømmer sneens morgen
+et drømmer helvedes morgen
 til endnu deles munden
 øjnene efterligne møde og støv
-med organerne endnu ligger organer
+med organerne endnu ligger orgasmer
 alt let forsvar drejer ved hundene
 men dagning og kærlighed eller kravlen og kravlen
 er stærkere uden det et-sagte land
-sproget er ikke lidt når
+sproget er ikke lidt hård
 personer personer hvad enten
 de leveres og ikke indføres simpelthen
 fraværet da er ophidselsens svar.""")
